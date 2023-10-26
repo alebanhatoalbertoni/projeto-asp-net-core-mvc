54be1cadfcb8a18800801aee4a4a7e792a2ee3d4
Alessandra Albertoni <alealbertoni@yahoo.com.br>
2023-10-26 15:52:08 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Services;
+
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        public IActionResult Index()
+        private readonly SellerService _sellerService;
+        //private readonly DepartmentService _departmentService;
+
+        public SellersController(SellerService sellerService)
         {
-            return View();
+            _sellerService = sellerService;
+            //_departmentService = departmentService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var list = await _sellerService.FindAllAsync();
+            return View(list);
+        }
+       
     }
 }
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using SalesWebMvc.Services.Exceptions;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<Seller>> FindAllAsync()
+        {
+            return await _context.Seller.ToListAsync();
+        }
+
+     }
+}
