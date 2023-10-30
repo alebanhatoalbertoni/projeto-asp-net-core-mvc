18611a3055ecab53ec9607f3ef6fcb98723b3d51
Alessandra Albertoni <alealbertoni@yahoo.com.br>
2023-10-30 16:14:46 -0300

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -16,10 +16,18 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-       public List<Seller> FindAll()
+        public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
 
+        public void Insert (Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
+       
+
+                                          
