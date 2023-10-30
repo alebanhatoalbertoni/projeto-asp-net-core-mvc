df7e42e16a14ca25dd9d9861898fbdd72fdfc53b
Alessandra Albertoni <alealbertoni@yahoo.com.br>
2023-10-30 16:27:35 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -32,6 +32,8 @@ namespace SalesWebMvc.Models
 
         public Departament Departament { get; set; }
 
+        public int DepartamentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
